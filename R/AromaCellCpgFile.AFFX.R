38ab158cba690f98fc4e0a93a1cc2354e693124b
Henrik Bengtsson <henrikb@braju.com>
2010-02-22 00:00:00 +0000

    version 1.5.0

--- R/AromaCellCpgFile.AFFX.R
+++ R/AromaCellCpgFile.AFFX.R
@@ -1,4 +1,4 @@
-setMethodS3("allocateFromCdf", "AromaCellMatchScoreFile", function(static, cdf, path=getPath(cdf), tags="*", ...) {
+setMethodS3("allocateFromCdf", "AromaCellCpgFile", function(static, cdf, path=getPath(cdf), tags="*", ...) {
   # Argument 'cdf':
   cdf <- Arguments$getInstanceOf(cdf, "AffymetrixCdfFile");
 
@@ -7,24 +7,25 @@ setMethodS3("allocateFromCdf", "AromaCellMatchScoreFile", function(static, cdf, 
   parts <- strsplit(chipType, split=",", fixed=TRUE)[[1]];
   tags[tags == "*"] <- paste(parts[-1], collapse=",");
   chipType <- parts[1];
-  chipType <- gsub(",monocell", "", chipType);
+
+  # Drop unwanted tags. AD HOC. /HB 2010-02-22
+  chipType <- gsub(",monocell", "", chipType, fixed=TRUE);
+
   platform <- getPlatform(cdf);
   nbrOfCells <- nbrOfCells(cdf);
   fullname <- paste(c(chipType, tags), collapse=",");
   ext <- getFilenameExtension(static);
   filename <- sprintf("%s.%s", fullname, ext);
   allocate(static, filename=filename, path=path, nbrOfCells=nbrOfCells, 
-                   platform=platform, chipType=chipType, ...);
+                                 platform=platform, chipType=chipType, ...);
 }, static=TRUE)
+
 
 
 ############################################################################
 # HISTORY:
 # 2009-02-22 [HB]
-# o Forgot to make allocateFromCdf() of AromaCellMatchScoreFile static.
-# 2009-02-10 [HB]
-# o Added optional validation of number of cells to byChipType().
-# o Static method byChipType() was not declared static.
-# 2008-10-28 [MR]
-# o Created from AromaCellSequenceFile.R.
+# o Forgot to make allocateFromCdf() of AromaCellCpgFile static.
+# 2010-02-19 [MR]
+# o Created from AromaCellPositionFile.AFFX.R.
 ############################################################################
