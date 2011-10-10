38660d4b2008d34de22ce4d42a525b01066bfedc
Henrik Bengtsson <henrikb@braju.com>
2011-10-10 00:00:00 +0000

    version 1.9.0

---
+++ inst/testScripts/system/chipTypes/HuEx-1_0-st-v2/test20101202,gcrma.R
@@ -0,0 +1,29 @@
+library("aroma.affymetrix");
+verbose <- Arguments$getVerbose(-3, timestamp=TRUE);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup data set
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+name <- "Affymetrix-HeartBrain";
+chipType <- "HuEx-1_0-st-v2";
+
+cdf <- AffymetrixCdfFile$byChipType(chipType, tags="coreR3,A20071112,EP");
+cdf <- AffymetrixCdfFile$byChipType(chipType, tags="fullR3,A20071112,EP");
+print(cdf);
+
+# Setup CEL set using the core CDF.
+csR <- AffymetrixCelSet$byName(name=name, cdf=cdf);
+print(csR);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Background correction and normalization
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdfS <- AffymetrixCdfFile$byChipType(getChipType(cdf, fullname=FALSE));
+setCdf(csR, cdfS);
+bc <- GcRmaBackgroundCorrection(csR, type="affinities");
+print(bc);
+csB <- process(bc, verbose=verbose);
+print(csB);
+setCdf(csB, cdf);
