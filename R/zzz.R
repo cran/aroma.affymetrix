77d08254817002e855a1a671b1ff1ef9210c98f2
Henrik Bengtsson <henrikb@braju.com>
2013-10-17 00:00:00 +0000

    version 2.11.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,35 +1,12 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE;
-
-# WORKAROUND: In order for the package to work with the most recent
-# version of R devel, which automatically add namespaces to packages
-# who do not have one, we explicitly have specify the following.
-# /HB 2011-07-27
-# R.utils:
-##cat <- R.utils::cat;
-##getOption <- R.utils::getOption;
-##lapply <- R.utils::lapply;
-
-# R.filesets:
-append <- R.filesets::append;
-##sapply <- R.filesets::sapply;
-
-# aroma.core:
-apply <- aroma.core::apply;
-colMeans <- aroma.core::colMeans;
-colSums <- aroma.core::colSums;
-library <- aroma.core::library;
-require <- aroma.core::require;
-.Machine <- aroma.core::.Machine;
+.onLoad <- function(libname, pkgname) {
+  ns <- getNamespace(pkgname);
+  pkg <- AromaAffymetrix(pkgname);
+  assign(pkgname, pkg, envir=ns);
+} # .onLoad()
 
 
 .onAttach <- function(libname, pkgname) {
-  pkg <- AromaAffymetrix(pkgname);
-  assign(pkgname, pkg, pos=getPosition(pkg));
-
-  # Setup package
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
   .setupAromaAffymetrix(pkg);
-
   startupMessage(pkg);
 }
